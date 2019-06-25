436064a32cf5c88a1d2fb98a746fcdd1df2c9c7c
SparkleX <sunxufei@hotmail.com>
2019-06-25 13:29:11 +0800

    add

--- c1-web/src/main/resources/public/core/view/CflDialog.js
+++ c1-web/src/main/resources/public/core/view/CflDialog.js
@@ -20,20 +20,11 @@ sap.ui.define([
 		var oView = this._oView;
 
 		this._oDialog = sap.ui.xmlfragment("next.core.view.CflDialog", this);
-	/*	Fragment.load({
-			id: "cfl",
-			name: "next.core.view.CflDialog",
-			controller: this
-		}).then(function (oDialog) {*/
         oView.addDependent(this._oDialog);
         var oModelList = new JSONModel();
         oModelList.loadData("/api/OCRD/");
-        /*oModelList.attachRequestCompleted(function() {
-            //console.log(oModelList.getData());
-        });*/
         this._oDialog.setModel(oModelList, "cfl");
         this._oDialog.open();
-		//});
 	};
 	theClass.prototype._handleSearch = function(oEvent) {
 		var sValue = oEvent.getParameter("value");
@@ -44,20 +35,12 @@ sap.ui.define([
 		var oBinding = oEvent.getSource().getBinding("items");
 		oBinding.filter([oFilter]);
 	}		
-	theClass.prototype._handleValueHelpClose = function (evt) {
-        var oSelectedItem = evt.getParameter("selectedItem");
-        if (oSelectedItem) {
-            var productInput = this.byId(this.inputId),
-                oText = this.byId('selectedKey'),
-                sDescription = oSelectedItem.getDescription();
-
-            productInput.setSelectedKey(sDescription);
-            oText.setText(sDescription);
-        }
-        //evt.getSource().getBinding("items").filter([]);
-       // this.setValue("1");
+	theClass.prototype._onClose = function (evt) {
     }	
-
+	theClass.prototype._onConfirm = function (evt) {
+        var oSelectedItem = evt.getParameter("selectedItem");
+        var item = oSelectedItem.getTitle();
+        this._oView.setData(item);
+    }
 	return theClass;
-
 });
