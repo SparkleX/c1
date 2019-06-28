014a0750611260cbbb75881c8dff69f1eaa04e21
sunxufei <sunxufei@hotmail.com>
2019-06-29 07:03:11 +0800

    quickview

--- c1-web/src/main/resources/public/core/widget/FormatLink.js
+++ c1-web/src/main/resources/public/core/widget/FormatLink.js
@@ -22,8 +22,8 @@ function(BaseClass, TextAlign,NumberFormat, CoreUtil, FormatUtil) {
     	if(format.right){
     		this.setTextAlign(TextAlign.Right);
     	}
-		this.setWidth("100%");
-		
+		//this.setWidth("100%");
+		this.attachPress(this.onPress);
     	return rt;
      }	
 	theClass.prototype.formatValue = function (value) {
@@ -37,14 +37,38 @@ function(BaseClass, TextAlign,NumberFormat, CoreUtil, FormatUtil) {
 	}
 	theClass.prototype.getText = function () {
 		return BaseClass.prototype.getText.call(this);
-	};	
+	}	
 	theClass.prototype.setDataValue = function (value) {
 
 		this.setText(value);
 		this.setProperty("dataValue", value);
-	};	
+	}	
 	theClass.prototype.getDataValue = function () {
 		return this.getText();
+	}	
+
+    theClass.prototype.openQuickView= function (oEvent, oModel) {
+		this.createPopover();
+
+		this._oQuickView.setModel(oModel);
+
+		// delay because addDependent will do a async rerendering and the actionSheet will immediately close without it.
+		var oButton = oEvent.getSource();
+		jQuery.sap.delayedCall(0, this, function () {
+			this._oQuickView.openBy(oButton);
+		});
+	};
+
+	theClass.prototype.onPress= function (evt) {
+		this.openQuickView(evt, null);
+	};
+
+	theClass.prototype.createPopover= function() {
+
+		if (!this._oQuickView) {
+			this._oQuickView = sap.ui.xmlfragment("next.share.quick.OCRD", this);
+			//this.getView().addDependent(this._oQuickView);
+		}
 	};	
 	return theClass;
 });
