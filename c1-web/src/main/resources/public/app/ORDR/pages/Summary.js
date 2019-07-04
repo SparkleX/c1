a2aca167cc36d10dec2c58616d269e7f261c46ca
SparkleX <sunxufei@hotmail.com>
2019-07-04 17:40:18 +0800

    add

--- c1-web/src/main/resources/public/app/ORDR/pages/Summary.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define(['sap/uxap/BlockBase'], function (BlockBase) {
-	"use strict";
-
-	return BlockBase.extend("next.app.ORDR.pages.Summary", {
-		metadata: {
-			/* no additional views provided */
-		}
-	});
-}, true);
